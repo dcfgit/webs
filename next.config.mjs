637c6ddc0eef26cebfaab01018feeaa530946fea
dcfgit <dcf-git@solutions-lo.com>
2025-12-17 09:28:57 -0300

    Limpieza de repositorio y archivos listos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',  // <--- Agrega esta línea
+  images: {
+    unoptimized: true, // GitHub Pages no soporta la optimización de imágenes nativa de Next.js
+  },
 };
 
 export default nextConfig;
